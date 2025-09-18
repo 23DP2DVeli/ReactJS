d5b027996268fa3a842bfd47e4d7a987572ce5a8
Daniels <A230255DV@rvt.lv>
2025-09-18 21:50:25 +0300

    methods in class App (+this.)

--- src/index.js
+++ src/index.js
@@ -2,11 +2,7 @@ import React from "react"
 import * as ReactDOMClient from 'react-dom/client';
 
 
-const inputClick = () => console.log("Clicked")
-const mouseOver = () => console.log("Mouse is ready!")
 
-
-const helpText = "Help text!"
 
 
 
@@ -19,17 +15,25 @@ class Header extends React.Component {
 }
 
 
-const App = () => {
-    return (<div className="name">
+class App extends React.Component {
+
+    helpText = "Help text!"
+    render() {
+        return (<div className="name">
         <Header />
 
 
-        <h1>{helpText}</h1>
-        <input placeholder={helpText}
-            onClick={inputClick} onMouseEnter={mouseOver}/>
-        <p>{helpText === "Help text!" ? "Yes" : "No"}</p>
+        <h1>{this.helpText}</h1>
+        <input placeholder={this.helpText}
+            onClick={this.inputClick} onMouseEnter={this.mouseOver}/>
+        <p>{this.helpText === "Help text!" ? "Yes" : "No"}</p>
 
 </div>)
+    }
+
+    inputClick () {console.log("Clicked")}
+    mouseOver () {console.log("Mouse is ready!")}
+
 }
 
 

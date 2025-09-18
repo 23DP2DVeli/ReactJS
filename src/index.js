4832adaa46ed3f2eed88c0cde7c0d8b969f94b67
Daniels <A230255DV@rvt.lv>
2025-09-18 21:54:12 +0300

    1 more file with export and import

--- src/index.js
+++ src/index.js
@@ -1,45 +1,6 @@
 import React from "react"
 import * as ReactDOMClient from 'react-dom/client';
-
-
-
-
-
-
-class Header extends React.Component {
-    render() {
-        return (
-        <header>{this.props.title}</header>
-    )
-    }
-}
-
-
-class App extends React.Component {
-
-    helpText = "Help text!"
-    render() {
-        return (<div className="name">
-
-
-        <Header title="Header" />
-
-
-        <h1>{this.helpText}</h1>
-        <input placeholder={this.helpText}
-            onClick={this.inputClick} onMouseEnter={this.mouseOver}/>
-        <p>{this.helpText === "Help text!" ? "Yes" : "No"}</p>
-
-</div>)
-    }
-
-    inputClick () {console.log("Clicked")}
-    mouseOver () {console.log("Mouse is ready!")}
-
-}
-
-
-
+import App from './App'
 
 
 

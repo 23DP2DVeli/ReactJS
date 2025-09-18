8ed9c5880f8590cf09fe72cf993c6586c7aa025f
Daniels <A230255DV@rvt.lv>
2025-09-18 21:57:01 +0300

    File for Header + File for App

--- src/App.js
+++ src/App.js
@@ -1,42 +1,20 @@
 import React from "react"
-
-
-
-
-
-
-class Header extends React.Component {
-    render() {
-        return (
-        <header>{this.props.title}</header>
-    )
-    }
-}
+import Header from ".components/Header.js"
 
 
 class App extends React.Component {
-
     helpText = "Help text!"
     render() {
         return (<div className="name">
-
-
         <Header title="Header" />
-
-
         <h1>{this.helpText}</h1>
         <input placeholder={this.helpText}
             onClick={this.inputClick} onMouseEnter={this.mouseOver}/>
         <p>{this.helpText === "Help text!" ? "Yes" : "No"}</p>
-
 </div>)
     }
-
     inputClick () {console.log("Clicked")}
     mouseOver () {console.log("Mouse is ready!")}
-
 }
-
-
 
 export default App 

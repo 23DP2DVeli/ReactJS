f04efeb4a65b61ae1a6e58eb9e163e0c71235819
Daniels <A230255DV@rvt.lv>
2025-09-18 21:42:15 +0300

    2 components

--- src/index.js
+++ src/index.js
@@ -2,26 +2,32 @@ import React from "react"
 import * as ReactDOMClient from 'react-dom/client';
 
 
-
-
-// ReactDOM.render(React.createElement('input',{
-//     placeholder: "Help text",
-//     onClick: () => console.log("Clicked"),
-//     onMouseEnter: () => console.log("Mouse is ready")
-// }), document.getElementById("app"))
-
 const inputClick = () => console.log("Clicked")
 const mouseOver = () => console.log("Mouse is ready!")
 
 
 const helpText = "Help text!"
 
+
+
+function Header () {
+    return (
+        <header>Header</header>
+    )
+}
+
+
+
+
 function App() {
     return (<div className="name">
-    <h1>{helpText}</h1>
-    <input placeholder={helpText}
-        onClick={inputClick} onMouseEnter={mouseOver}/>
-    <p>{helpText === "Help text!" ? "Yes" : "No"}</p>
+        <Header />
+
+        
+        <h1>{helpText}</h1>
+        <input placeholder={helpText}
+            onClick={inputClick} onMouseEnter={mouseOver}/>
+        <p>{helpText === "Help text!" ? "Yes" : "No"}</p>
 
 </div>)
 }
